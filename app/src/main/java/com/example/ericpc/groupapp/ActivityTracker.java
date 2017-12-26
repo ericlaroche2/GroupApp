f55d2553e0cd1611a07bca0f2121719cecf8c514
ElieMassaad <eliemassaad0@gmail.com>
2017-12-26 15:00:28 -0500

    Finished ActivityTracker layout

---
+++ app/src/main/java/com/example/ericpc/groupapp/ActivityTracker.java
@@ -0,0 +1,13 @@
+package com.example.ericpc.groupapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTracker extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+    }
+}
