2cd98a0ea4963157f1b46ebf848b733ad81f87d8
ElieMassaad <eliemassaad0@gmail.com>
2018-01-03 18:27:41 -0500

    Want to add ASYNK

--- app/src/main/java/com/example/ericpc/groupapp/Progress.java
+++ app/src/main/java/com/example/ericpc/groupapp/Progress.java
@@ -5,10 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-
+import  android.support.design.widget.Snackbar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,6 +26,7 @@ public class Progress extends AppCompatActivity {
     private EditText previousMonth;
     private EditText currentMont;
     private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,53 +61,77 @@ public class Progress extends AppCompatActivity {
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy, HH:mm");
         String date = df.format(Calendar.getInstance().getTime());
 
-        String month = date.substring(3,5);
+        String month = date.substring(3, 5);
 
         int currentMonth = Integer.parseInt(month);
         int previousMonthnum = 0;
 
-        if(currentMonth == 1){
+        if (currentMonth == 1) {
             previousMonthnum = 12;
 
-        }else{
-            previousMonthnum = currentMonth - 1 ;
+        } else {
+            previousMonthnum = currentMonth - 1;
         }
 
-        double  lastMonthMinutes = 0;
+        double lastMonthMinutes = 0;
         double thisMonthMinutes = 0;
 
-        for(int i=0; i < dateArray.size(); i++){
-         String dateInArray =   dateArray.get(i);
-          int  monthInArray = Integer.parseInt(dateInArray.substring(3,5));
+        for (int i = 0; i < dateArray.size(); i++) {
+            String dateInArray = dateArray.get(i);
+            int monthInArray = Integer.parseInt(dateInArray.substring(3, 5));
 
-            if(monthInArray == previousMonthnum){
+            if (monthInArray == previousMonthnum) {
                 lastMonthMinutes += minutesArray.get(i);
 
-            }else if(monthInArray == currentMonth ){
+            } else if (monthInArray == currentMonth) {
 
-                thisMonthMinutes  += minutesArray.get(i);
+                thisMonthMinutes += minutesArray.get(i);
             }
 
         }
 
-       progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar);
         previousMonth = findViewById(R.id.previous);
         currentMont = findViewById(R.id.current);
 
+        previousMonth.setEnabled(false);
+        currentMont.setEnabled(false);
         previousMonth.setText(Double.toString(lastMonthMinutes));
         currentMont.setText(Double.toString(thisMonthMinutes));
 
-      double howMuchFarher;
+        double howMuchFarher;
+        double percent = 0;
+        View view = findViewById(R.id.toolbar);
 
-      if(thisMonthMinutes >= lastMonthMinutes){
-          progressBar.setProgress(100);
-
-      }else{
-          double percent = (thisMonthMinutes / lastMonthMinutes)*100;
-          progressBar.setProgress((int)percent);
+        if (thisMonthMinutes >= lastMonthMinutes) {
+            progressBar.setProgress(100);
+            Snackbar.make(view, "Great Job! You've worked out more this month!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        } else {
+            percent = (thisMonthMinutes / lastMonthMinutes) * 100;
+            progressBar.setProgress((int) percent);
         }
 
 
+        if (percent > 75 && percent < 100) {
+            Snackbar.make(view, "You are almost there!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        } else if (percent > 50 && percent < 75) {
+            Snackbar.make(view, "Don;t stop now!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        } else if (percent == 50) {
+            Snackbar.make(view, "You are half way there!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        } else if (percent < 50) {
+            Snackbar.make(view, "You will get there!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
         }
+
+
     }
 
+
+}
